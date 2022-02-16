92b4ab3b19de10f1766c7a01abc726b2ac5ade75
jenkins <jenkins@localhost.localdomain>
2022-02-16 13:43:48 -0500

    tird commit

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
+WORKSPACE=/home/jenkins/jenkinsvolume/workspace/pipeline-maven
 
-
-docker run --rm  -v /home/jenkins/jenkinshome/pipeline/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
 
