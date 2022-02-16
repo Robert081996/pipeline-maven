860d07f82f339a52c0e13f851af7f947e13171d8
jenkins <jenkins@localhost.localdomain>
2022-02-16 14:19:38 -0500

    deploy files changed

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/jenkins/prod /tmp/.auth centos@13.232.24.77:/tmp/.auth
-scp -i /home/jenkins/prod ./jenkins/deploy/publish.sh centos@13.232.24.77:/tmp/publish.sh
-ssh -i /home/jenkins/prod centos@13.232.24.77 "/tmp/publish.sh"
+scp -i /opt/prod /tmp/.auth centos@13.232.24.77:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh centos@13.232.24.77:/tmp/publish.sh
+ssh -i /opt/prod centos@13.232.24.77 "/tmp/publish.sh"
