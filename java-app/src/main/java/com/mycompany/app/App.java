a5de27dd508795bbd37854e4d129b879b7b5a707
jenkins <jenkins@localhost.localdomain>
2022-02-17 04:50:37 -0500

    Content changed

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,12 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * Hello world from Jenkins!
  */
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World from Jenkins!";
 
     public App() {}
 
